4c7591366de209f2a1d7238d83f3f132bd5d1454
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2025-12-24 00:07:56 +0600

    generic section added

---
+++ generic.ts
@@ -0,0 +1,20 @@
+// storage.ts
+class DataStorage<T> {
+    private data: T[] = [];
+
+    addItem(item: T) {
+        this.data.push(item);
+    }
+
+    removeItem(item: T) {
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+
+    getItems() {
+        return [...this.data];
+    }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem("TypeScript is awesome!");
+console.log(textStorage.getItems());
